849fb54b0b012aa27a4680d1b2a02ab7d4fa4ff8
Steven Cooper <steven.cooper@securenettech.com>
2020-12-15 23:49:17 -0500

    Added screenshots

--- src/employeeManager.test.ts
+++ src/employeeManager.test.ts
@@ -7,6 +7,7 @@ const driver: WebDriver = new Builder()
 let test1 = new ManagerPage(driver, 123456, "ABC", "ABCD", "Billy", "M", "WHT", 
 603, 220, "BLK", "Battery", 11192019, "C160098765", "FL", 11192020, "BNPY81", "FL", 2020);
 
+const fs = require("fs")
 test("it works", async () => {
   await test1.navigate();
   expect(await driver.findElement(test1.clearButton)).toBeTruthy();
@@ -16,10 +17,19 @@ test("It fills out the form", async() =>{
   expect(await (await driver.findElement(test1.submitButton)).isEnabled()).toBeTruthy();
 });
 // This test is running with two many numbers for field "Header" which should
-// Return an error message.
+// Return an error message. This references SCDL-25.
 test("It displays an error message when fields are invalid", async() => {
   await test1.submitForm();
   expect(await test1.checkForErrors()).toBeTruthy;
+  await fs.writeFile(
+    `${__dirname}/../screenshots/error.png`,
+    await test1.driver.takeScreenshot(),
+    "base64",
+    (e) => {
+      if (e) console.error(e);
+      else console.log("Screenshot Saved!");
+    }
+  )
 });
 afterAll(async () => {
   await driver.quit();
